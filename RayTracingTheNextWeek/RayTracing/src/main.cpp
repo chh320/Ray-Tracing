1c4e901cbe0e80acf7f226f67077be80d2597859
chh320 <513860438@qq.com>
2022-10-17 21:04:08 +0800

    update

--- RayTracingTheNextWeek/RayTracing/src/main.cpp
+++ RayTracingTheNextWeek/RayTracing/src/main.cpp
@@ -96,21 +96,6 @@ int main()
 	//auto R = cos(pi / 4);
 	auto world = random_scene();
 
-	//auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	//auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	//auto material_left   = make_shared<dielectric>(1.5);
-	//auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-	//auto material_left = make_shared<lambertian>(color(0, 0, 1));
-	//auto material_right = make_shared<lambertian>(color(1, 0, 0));
-
-	//world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-	//world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-	//world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-	//world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
-	//world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-	//world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-	//world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
-
 	// Camera
 	point3 lookfrom(13, 2, 3);
 	point3 lookat(0, 0, 0);
